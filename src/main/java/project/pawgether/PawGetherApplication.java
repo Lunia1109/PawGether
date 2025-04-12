a7c3f17efa6fa434f4ebcb5bdb96190568ed5fe5
luther <lunia1109@naver.com>
2025-04-12 23:46:59 +0900

    초기 프로젝트 설정 및 Hello World 컨트롤러 추가

---
+++ src/main/java/project/pawgether/PawGetherApplication.java
@@ -0,0 +1,13 @@
+package project.pawgether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawGetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawGetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/pawgether/PawGetherApplicationTests.java
@@ -0,0 +1,13 @@
+package project.pawgether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawGetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
